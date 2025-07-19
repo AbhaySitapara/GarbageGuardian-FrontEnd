9d62465a71ca21a139f3ea9711fac9f84ce954e6
Abhay Sitapara <141837593+AbhaySitapara@users.noreply.github.com>
2025-07-19 12:33:39 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BACKEND_API_ENDPOINT = "http://localhost:3001"
+const BACKEND_API_ENDPOINT = "https://garbageguardian-backend-ne6a.onrender.com"
 
 //Het's Hosted Backend Link : https://garbageguardian-backend.onrender.com
 
